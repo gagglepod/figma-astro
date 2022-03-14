2102713a78652d6a0ca8920d6b6892b3e8343ab9
Kyle Bondo <gagglepod@gmail.om>
2022-03-14 08:36:01 -0500

    Base Layout and Components ready.

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+const postcssjitprops = require("postcss-jit-props");
+const OpenProps = require("open-props");
+
+module.exports = {
+  plugins: [postcssjitprops(OpenProps), require("autoprefixer")],
+};
